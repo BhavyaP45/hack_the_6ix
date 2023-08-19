11bac3980ee8363342719d13d35040be3a0100d3
Bhavya Patel <127996743+BhavyaP45@users.noreply.github.com>
2023-08-19 07:10:30 -0400

    Create Login Form with Flask WT-Forms

--- docs/__init__.py
+++ docs/__init__.py
@@ -1,9 +1,26 @@
-from flask import Flask
+#Import modules and libraries that will be used throughout the different files
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
+from flask_wtf import FlaskForm
 
+#Create app
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///test.db' #Configure a DB using SQLite
+app.config['SECRET_KEY'] = 'e5c9095100a0ab1d7cb9a75f' #Configure a security key to ensure a secure connection
 
+#Create a database model
+db = SQLAlchemy()
+db.init_app(app)
+
+#Bcrypt class to store hashed passwords
+bcrypt = Bcrypt(app)
+
+#Use flask_login built in login class for users
+loginmanager = LoginManager(app)
+loginmanager.login_view = "login_page" #When unauthorized user tries clicking the market button, gets redirected
+
+#Obtain routes from routes.py in the docs module
 from docs import routes
--- docs/routes.py
+++ docs/routes.py
@@ -1,5 +1,53 @@
-from docs import app
+from docs import app, render_template, redirect, db, url_for, flash, login_user, logout_user, bcrypt, current_user
+from docs.forms import RegisterForm, LoginForm, LogoutForm
+from docs.models import User
 
 @app.route("/")
-def home_page():
-    return "Hello World"
+def index():
+    return render_template("index.html")
+@app.route("/register", methods = ["POST", "GET"])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username = form.username.data, email_address = form.email_address.data, password = form.password.data, is_manager = False)
+        with app.app_context():
+            db.session.add(user_to_create)
+            db.session.commit()
+            login_user(user_to_create)
+
+        return redirect(url_for("dashboard_page"))
+    
+    if form.errors != {}: #.errors Stores all the errors from the form (If there are no errors from the validations)
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category = "error")
+
+    return render_template("register.html", form = form)
+
+@app.route("/dashboard", methods = ["POST", "GET"])
+def dashboard_page():
+    
+    return render_template("dashboard.html")
+
+@app.route('/login', methods = ["GET", "POST"])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        try:
+            attempted_user = User.query.filter_by(username = form.username.data).first() 
+
+            #Check if the user is already stored in the database and check password with password hash
+            if attempted_user and attempted_user.check_password_correction(form.password.data):
+                login_user(attempted_user)
+                flash(f'Welcome Back {attempted_user.username}', category="success")
+
+                return redirect(url_for("dashboard"))
+            else:
+                flash("User name and password are not matched!", category= "error")
+        except:
+            flash("Something Went Wrong", category = "error")
+    return render_template('login.html', form = form)
+
+@app.route('/logout', methods = ["POST","GET"])
+def logout_page():
+    logout_user()
+    return redirect(url_for("index"))
