5b910d447a091d75215cf04d5fe51e7db37ebf11
Solaror0 <133717438+Solaror0@users.noreply.github.com>
2023-08-19 11:02:16 -0400

    Selectively merged files
    Selectively merged some files and code snippers from bhav flask branch into main branch so as to not cause conflicts and code loss.

---
+++ docs/forms.py
@@ -0,0 +1,34 @@
+#Import wtf modules for the specific login forms
+from docs import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+
+#Obtain user class from models
+from docs.models import User
+
+class RegisterForm(FlaskForm):
+    #specific function naming convention so flask can execute it
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username = username_to_check.data).first()
+
+        if user:
+            raise ValidationError("Username already exists!")
+        
+    def validate_email_address(self, address_to_check):
+        user = User.query.filter_by(email_address = address_to_check.data).first()
+
+        if user:
+            raise ValidationError("Email already used!")    
+        
+    username = StringField(label = "Username", validators = [Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label = "Email", validators = [Email(), DataRequired()])
+    password = PasswordField(label = "Password", validators = [Length(min=6), DataRequired()])
+    password_confirm = PasswordField(label = "Password Confirmation", validators = [EqualTo('password'), DataRequired()])
+    is_manager_field = BooleanField(label = "Are you a manager for your company?")
+    submit = SubmitField(label = "Create Account")
+
+class LoginForm(FlaskForm): 
+    username = StringField(label = "Username", validators = [Length(min=2, max=30), DataRequired()])
+    password = PasswordField(label = "Password", validators = [Length(min=6), DataRequired()])
+    submit = SubmitField(label = "Login")
+
--- docs/routes.py
+++ docs/routes.py
@@ -1,9 +1,66 @@
 from docs import app, Flask, redirect, url_for, request, render_template, flash, sqlite3
+from docs import app, render_template, redirect, db, url_for, flash, login_user, logout_user, bcrypt, current_user
+from docs.forms import RegisterForm, LoginForm
+from docs.models import User
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+
+@app.route("/register", methods = ["POST", "GET"])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        try:
+            user_to_create = User(username = form.username.data, email_address = form.email_address.data, password = form.password.data, is_manager = form.is_manager_field.data)
+            with app.app_context():
+                db.session.add(user_to_create)
+                db.session.commit()
+                login_user(user_to_create)
+        
+            return redirect(url_for("dashboard_page"))
+        
+        except:
+            flash(f'There was an error with creating a user: {err_msg}', category = "error")
+    
+    elif form.errors != {}: #.errors Stores all the errors from the form (If there are no errors from the validations)
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category = "error")
+
+    return render_template("register.html", form = form)
+
+@app.route("/dashboard", methods = ["POST", "GET"])
+def dashboard_page():
+    
+    return render_template("dashboard.html")
+
+@app.route('/login', methods = ["GET", "POST"])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        try:
+            attempted_user = User.query.filter_by(username = form.username.data).first() 
+
+            #Check if the user is already stored in the database and check password with password hash
+            if attempted_user and attempted_user.check_password_correction(form.password.data):
+                login_user(attempted_user)
+                flash(f'Welcome Back {attempted_user.username}', category="success")
+
+                return redirect(url_for("dashboard_page"))
+            else:
+                flash("User name and password are not matched!", category= "error")
+        except:
+            flash("Something Went Wrong", category = "error")
+
+    return render_template('login.html', form = form)
+
+@app.route('/logout', methods = ["POST","GET"])
+def logout_page():
+    logout_user()
+    flash("Success", category= "success")
+    return redirect(url_for("index"))
 
 
 @app.route("/announcements", methods=['POST', 'GET'])
